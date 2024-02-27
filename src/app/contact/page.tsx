c620f5ef7abceca44b48f63cac25ba99a32724e8
Stasya Geleisha <s.geleisha@gmail.com>
2024-02-27 13:41:44 +0200

    Contact Page Design

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,11 +3,30 @@ import styles from "./contact.module.scss";
 
 const Contact = () => {
   return (
-    <div className={styles.wrapper}>
-      <div>Form</div>
-      <div>
-        <Image alt="Contact" src={"/assets/contact.png"} fill />
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <form className={styles.form}>
+          <input type="text" placeholder="Name and surname" />
+          <input type="email" placeholder="Email addresses" />
+          <input type="tel" placeholder="Phone number (optional)" />
+          <textarea
+            name="text"
+            cols={30}
+            rows={20}
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
       </div>
+      <>
+        <Image
+          className={styles.item}
+          alt="Contact"
+          src={"/assets/contact.png"}
+          width={500}
+          height={500}
+        />
+      </>
     </div>
   );
 };
